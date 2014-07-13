6a937ae5e303dfd37273899bc21bb3f4cded0173
nguyenchihoang <nguyenchihoangapple@gmail.com>
2014-07-13 23:24:47 +0700

    asg02

---
+++ src/main/java/opp/asg02demo/App.java
@@ -0,0 +1,8 @@
+package opp.asg02demo;
+
+public class App {
+	public static void main( String[] args )    {
+	    BigInteger number = new BigInteger(10);
+        System.out.println( "Hello World!" + number.toString() );
+    }
+}
---
+++ src/main/java/opp/asg02demo/BigInteger.java
@@ -0,0 +1,32 @@
+package opp.asg02demo;
+
+public class BigInteger {
+	 public BigInteger(int init) {
+	        
+	    }
+
+	    public BigInteger(String init) {
+	        
+	    }
+
+	    public String toString() {
+	        
+	        return "";
+	    }
+
+	    public boolean equals(Object other) {
+	        return toString().equals(other.toString());
+	    }
+
+	    public long toLong() {
+	        return 0;  
+	    }
+
+	    public String add(BigInteger other) {
+	        return other.toString(); 
+	    }
+
+	    public String subtract(BigInteger other) {
+	        return other.toString();  
+	    }
+}
